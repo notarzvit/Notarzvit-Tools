b3297cc55df4a7b9a3247a73c44361ca15e00f86
Anton Andriyevskyy <x.meglio@gmail.com>
2011-11-25 22:19:39 +0200

    Add rebuild script

---
+++ rebuild.php
@@ -0,0 +1,36 @@
+<?php
+
+function out($msg, $lnNum=1) { fwrite(STDOUT, $msg.str_repeat("\n",$lnNum)); }
+function ln($n=1) { out('', $n); }
+function h1($msg) { ln(2); out("-------");  out($msg); }
+
+if (!file_exists('config.php')) { out('Cannot find config.php file!'); die(); }
+require_once 'config.php';
+
+
+h1("Update sources from git...");
+chdir($dir);
+system("git pull");
+system("git submodule update --init --recursive");
+
+
+h1("Rebuild Propel ORM classes...");
+chdir($dir."/core/orm");
+system("../../lib/propel/generator/bin/propel-gen");
+
+
+h1("Set write permissions for folders...");
+$writeDirs = array(
+	$dir.'/lib/tcpdf/cache',
+	$dir.'/lib/tcpdf/fonts',
+	$dir.'/lib/tcpdf/images',
+	$dir.'/docs/invoices'
+);
+foreach($writeDirs as $d)
+{
+	out('  * '.$d);
+	chmod($d, 0776);
+}
+
+
+ln(); out("Done!", 2);
